1e833341a59e685d918650fb8c6575224603451b
Salvini126 <salvini.matteo@iisgalvanimi.edu.it>
2022-05-08 20:35:53 +0000

    punto 3

--- salvini-booking/src/app/app.component.ts
+++ salvini-booking/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
   title = 'salvini-booking';
+  obsRoom : Observable <object> = undefined!;
+  data : object = undefined!;
+  
+  constructor(private http : HttpClient){
+    
+  }
+  ngOnInit(): void {
+    this.obsRoom = this.http.get<object>('https://my-json-server.typicode.com/malizia-g/hotel/short-booking')
+    this.obsRoom.subscribe(this.passaDati)
+  }
+  passaDati = (data: object) => {
+    this.data = data;
+    console.log(data)
+  }
+  
 }
